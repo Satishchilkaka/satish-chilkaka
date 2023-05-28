25290346f28757a09aaafbcd9a266d90c2801ffa
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-05-28 15:51:05 -0400

    add profile header

---
+++ src/app/profile/about-me/about-me.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.scss']
+})
+export class AboutMeComponent {
+
+
+  ngOnInit() {
+    // this.about1 =  this.profileService.about
+    // this.about2 =  this.profileService.about2
+  }
+}
