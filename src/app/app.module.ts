8be0c00cb14211425c850a2b14f2ccbf65c018d6
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-05-28 15:51:27 -0400

    update styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutMeComponent } from './profile/about-me/about-me.component';
+import { HeaderComponent } from './profile/header/header.component';
+import { FooterComponent } from './profile/footer/footer.component';
+import { AboutComponent } from './profile/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AboutMeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
